b3de695f3224599e54b3d93537c33599668798a8
Ahmed Maher <ahmedelseaidy22@gmail.com>
2023-08-08 05:09:29 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,53 @@
-import React from 'react';
-import './App.css';
-import 'h8k-components';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import "h8k-components";
 
-import Articles from './components/Articles';
+import Articles from "./components/Articles";
 
 const title = "Sorting Articles";
 
-function App({articles}) {
+function App({ articles }) {
+  const [artic, setArtic] = useState(articles);
 
-  const sortUpvoted = (arti) =>{
-    
-  }
-  const mostUpvoted = () => {
-    articles.
-  }
-    return (
-        <div className="App">
-            <h8k-navbar header={title}></h8k-navbar>
-            <div className="layout-row align-items-center justify-content-center my-20 navigation">
-                <label className="form-hint mb-0 text-uppercase font-weight-light">Sort By</label>
-                <button data-testid="most-upvoted-link" className="small">Most Upvoted</button>
-                <button data-testid="most-recent-link" className="small">Most Recent</button>
-            </div>
-            <Articles articles={articles}/>
-        </div>
-    );
+  const sortUpvoted = () => {
+    const sortedArticles = [...artic].sort((a, b) => b.upvotes - a.upvotes);
+    setArtic(sortedArticles);
+  };
 
+  const sortRecent = () => {
+    const sortedArticles = [...artic].sort((a, b) => new Date(b.date) - new Date(a.date));
+    setArtic(sortedArticles);
+  };
+
+  useEffect(() => {
+    sortUpvoted()
+  }, [])
+  
+  return (
+    <div className="App">
+      <h8k-navbar header={title}></h8k-navbar>
+      <div className="layout-row align-items-center justify-content-center my-20 navigation">
+        <label className="form-hint mb-0 text-uppercase font-weight-light">
+          Sort By
+        </label>
+        <button
+          data-testid="most-upvoted-link"
+          className="small"
+          onClick={() => sortUpvoted()}
+        >
+          Most Upvoted
+        </button>
+        <button
+          data-testid="most-recent-link"
+          className="small"
+          onClick={() => sortRecent()}
+        >
+          Most Recent
+        </button>
+      </div>
+      <Articles articles={artic} />
+    </div>
+  );
 }
 
 export default App;
